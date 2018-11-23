d004e66d9cc1d0b1ce6f353bd1a22d2bcdea203f
Pong <samfunghp@gmail.com>
2018-11-24 00:07:22 +0800

    make rollup config better

--- rollup.config.js
+++ rollup.config.js
@@ -5,141 +5,95 @@ import uglify from "rollup-plugin-uglify";
 import scss from "rollup-plugin-scss";
 import CleanCSS from "clean-css";
 import fs from "fs";
+import path from "path";
 
 const dev = process.env.NODE_ENV === "dev";
 
-const rollupOutput = options => ({
-  globals: {
-    "video.js": "videojs"
-  },
-  banner: `/* eslint-disable */`,
-  ...options
+const mkdirp = dir =>
+  path
+    .resolve(dir)
+    .split(path.sep)
+    .reduce((acc, cur) => {
+      const currentPath = path.normalize(acc + path.sep + cur);
+      try {
+        fs.statSync(currentPath);
+      } catch (e) {
+        if (e.code === "ENOENT") {
+          fs.mkdirSync(currentPath);
+        } else {
+          throw e;
+        }
+      }
+      return currentPath;
+    }, "");
+
+const createEntry = ({ input, output, css = true }) => {
+  return {
+    input,
+    output: {
+      globals: {
+        "video.js": "videojs"
+      },
+      ...output
+    },
+    external: ["video.js"],
+    plugins: [
+      resolve({
+        jsnext: true,
+        main: true,
+        browser: true
+      }),
+      commonjs({
+        sourceMap: false
+      }),
+      scss({
+        output: styles => {
+          if (styles.length && css) {
+            const cssOutput = output.file.replace(".min.js", ".css");
+            const parsedCSS = new CleanCSS().minify(styles);
+
+            mkdirp(cssOutput.replace(/[^\/]*$/, ""));
+
+            fs.writeFileSync(cssOutput, parsedCSS.styles);
+          }
+        }
+      }),
+      babel({
+        babelrc: true,
+        exclude: "node_modules/**",
+        compact: false
+      }),
+      !dev && uglify()
+    ]
+  };
+};
+
+const coreEntries = ["umd", "cjs"].map(format => {
+  const prefix = format === "umd" ? "" : `.${format}`;
+
+  return createEntry({
+    input: "source/index.js",
+    output: {
+      file: `dist/videojs-plus${prefix}.min.js`,
+      format
+    },
+    css: format === "umd"
+  });
 });
 
-const scssConfig = (dest, createIndexJS) => {
-  return scss({
-    output: function(styles) {
-      const css = new CleanCSS().minify(styles);
-      if (styles.length) {
-        fs.writeFileSync(dest, css.styles);
-        createIndexJS && createIndexJS(true);
-      } else {
-        createIndexJS && createIndexJS(false);
-      }
+const pluginEntries = fs.readdirSync("source/Plugin").map(pluginName => {
+  const parsedName = pluginName
+    .split(/(?=[A-Z])/)
+    .join("-")
+    .toLowerCase();
+
+  return createEntry({
+    input: `source/Plugin/${pluginName}/${pluginName}.js`,
+    output: {
+      file: `dist/${parsedName}/videojs-plus.${parsedName}.min.js`,
+      format: "iife"
     }
   });
-};
+});
 
-const external = ["video.js", "videojs-contrib-ads"];
-
-const rollupPlugins = [
-  resolve({
-    jsnext: true,
-    main: true,
-    browser: true
-  }),
-  commonjs({
-    sourceMap: false
-  }),
-  babel({
-    babelrc: false,
-    exclude: "node_modules/**",
-    compact: false,
-    presets: [
-      [
-        "@babel/preset-env",
-        {
-          loose: true,
-          modules: false
-        }
-      ]
-    ]
-  }),
-  !dev && uglify()
-];
-
-const vjsPlusPlugins = () => {
-  const vjsPlusPlugins_ = [
-    {
-      name: "playlist",
-      input: "source/Plugin/Playlist/Playlist.js"
-    },
-    {
-      name: "autoplay",
-      input: "source/Plugin/AutoPlay.js"
-    },
-    {
-      name: "full-window-toggle",
-      input: "source/Plugin/FullWindowToggle/FullWindowToggle.js"
-    },
-    {
-      name: "subtitles",
-      input: "source/Plugin/Subtitles/Subtitles.js"
-    },
-    {
-      name: "quality",
-      input: "source/Plugin/Quality/Quality.js"
-    },
-    {
-      name: "quality-hls",
-      input: "source/Plugin/Quality/Quality.Hls.js"
-    },
-    {
-      name: "picture-in-picture",
-      input: "source/Plugin/PictureInPicture/PictureInPicture.js"
-    },
-    {
-      name: "live",
-      input: "source/Plugin/Live/Live.js"
-    }
-  ];
-
-  return vjsPlusPlugins_.map(({ name, input }) => {
-    const dir = `dist/plugins/${name}/`;
-
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir);
-    }
-
-    const jsFileName = `videojs-plus-${name}.min.js`;
-    const cssFileName = `videojs-plus-${name}.min.css`;
-    const createIndexJS = css => {
-      const content = `
-      import './${jsFileName}'\n${css ? `import './${cssFileName}'` : ""}
-      `;
-
-      fs.writeFileSync(`${dir}/index.js`, content.trim());
-    };
-
-    return {
-      input,
-      output: [
-        rollupOutput({
-          file: `${dir}/${jsFileName}`,
-          format: "iife"
-        })
-      ],
-      external,
-      plugins: [scssConfig(`${dir}/${cssFileName}`, createIndexJS), ...rollupPlugins]
-    };
-  });
-};
-
-export default [
-  {
-    input: "source/index.js",
-    output: [
-      rollupOutput({
-        file: `dist/videojs-plus.min.js`,
-        format: "umd"
-      }),
-      rollupOutput({
-        file: `dist/videojs-plus.cjs.min.js`,
-        format: "cjs"
-      })
-    ],
-    external,
-    plugins: [scssConfig("dist/videojs-plus.min.css"), ...rollupPlugins]
-  },
-  ...vjsPlusPlugins()
-];
+export default [...coreEntries, ...pluginEntries];
