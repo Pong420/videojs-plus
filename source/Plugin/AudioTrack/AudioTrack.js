86d2a46cb26e290843007c0516cea1732a415a07
Pong <samfunghp@gmail.com>
2018-11-26 22:33:09 +0800

    audio track should be done

--- source/Plugin/AudioTrack/AudioTrack.js
+++ source/Plugin/AudioTrack/AudioTrack.js
@@ -1,4 +1,4 @@
-import { hook, getPlugin, registerPlugin } from 'video.js';
+import { getPlugin, registerPlugin } from 'video.js';
 import './AudioSettingItem';
 
 class audio extends getPlugin('plugin') {
@@ -6,6 +6,12 @@ class audio extends getPlugin('plugin') {
     super(player, options);
 
     this.current = this.values().find(track => track.enabled);
+
+    // I am worried about audio changed by other factor
+    // So also listen on `audiochange` and update the value
+    player.on('audiochange', (_, { index }) => {
+      this.current = this.values()[index];
+    });
   }
 
   values() {
@@ -32,31 +38,4 @@ class audio extends getPlugin('plugin') {
   }
 }
 
-hook('setup', vjsPlayer => {
-  const hls = vjsPlayer.tech().hls;
-
-  hls.on('hls-demuxed', evt => console.log(evt));
-  hls.on('hls-alternate-audio	', evt => console.log(evt));
-  hls.on('hls-audio-change', evt => console.log(evt));
-
-  vjsPlayer.on('loadedmetadata', () => {
-    const { component: AudioSettingItem } = vjsPlayer.findChild('AudioSettingItem')[0];
-    const audioTracks = vjsPlayer.audio().value();
-
-    AudioSettingItem.setEntries(
-      audioTracks.map(({ id, kind, label, language }, index) => ({
-        id,
-        kind,
-        label,
-        language,
-        index
-      }))
-    );
-
-    AudioSettingItem.show();
-  });
-});
-
 registerPlugin('audio', audio);
-
-export default audio;
