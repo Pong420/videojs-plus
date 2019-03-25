2c07e13fc794e7932ea707a461d7ff09078fba21
Pong <samfunghp@gmail.com>
2019-03-25 23:50:14 +0800

    update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,58 +1,95 @@
+import fs from 'fs';
+import path from 'path';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import sourceMaps from 'rollup-plugin-sourcemaps';
-import camelCase from 'lodash/camelCase';
 import scss from 'rollup-plugin-scss';
 import json from 'rollup-plugin-json';
+import kebabCase from 'lodash/kebabCase';
 
-const pkg = require('./package.json');
+function createEntry(input, output, css) {
+  return {
+    input,
+    output,
+    // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+    external: ['video.js'],
+    plugins: [
+      // Allow json resolution
+      json(),
+      // Compile TypeScript files
+      // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+      commonjs(),
+      // Allow node_modules resolution, so you can use 'external' to control
+      // which external modules to include in the bundle
+      // https://github.com/rollup/rollup-plugin-node-resolve#usage
+      resolve(),
+      babel({
+        babelrc: true,
+        exclude: 'node_modules/**',
+        compact: false
+      }),
 
-export default {
-  input: `source/index.js`,
-  output: [
-    {
-      file: pkg.main,
-      name: camelCase(pkg.name),
-      format: 'umd',
+      // Resolve source maps to the original source
+      sourceMaps(),
+      scss({
+        output: styles => {
+          if (styles && styles.length) {
+            const cssOutput = css || output[0].file.replace(/\.(umd|es|iife).js/, '.css');
+            mkdirp(cssOutput.replace(/[^\/]*$/, ''));
+            fs.writeFileSync(cssOutput, styles);
+          }
+        }
+      })
+    ]
+  };
+}
+
+const pluginsDir = 'source/Plugin';
+const output = setFileName =>
+  ['umd', 'es'].map(format => {
+    return {
+      file: setFileName(format),
+      format,
       sourcemap: true,
       globals: {
         'video.js': 'videojs'
       }
-    },
-    { file: pkg.module, format: 'es', sourcemap: true },
-    {
-      file: pkg.module,
-      format: 'iife',
-      sourcemap: true,
-      globals: {
-        'video.js': 'videojs'
-      }
-    }
-  ],
-  // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: ['video.js'],
-  watch: {
-    include: 'source/**'
-  },
-  plugins: [
-    // Allow json resolution
-    json(),
-    // Compile TypeScript files
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
-    scss(),
-    babel({
-      babelrc: true,
-      exclude: 'node_modules/**',
-      compact: false
-    }),
+    };
+  });
 
-    // Resolve source maps to the original source
-    sourceMaps()
-  ]
-};
+export default [
+  createEntry('source/index.js', output(format => `dist/videojs-plus.${format}.js`)),
+  ...fs.readdirSync(pluginsDir).map(pluginName => {
+    const kebabCaseName = kebabCase(pluginName);
+    const outdir = `dist/plugins/${kebabCaseName}`;
+    return createEntry(
+      `${pluginsDir}/${pluginName}/${pluginName}.js`,
+      output(format => {
+        const name = format === 'umd' ? 'index' : `${kebabCaseName}.${format}`;
+        return `${outdir}/${name}.js`;
+      }),
+      `${outdir}/style.css`
+    );
+  })
+];
+
+function mkdirp(dir) {
+  return path
+    .resolve(dir)
+    .split(path.sep)
+    .reduce((acc, cur) => {
+      const currentPath = path.normalize(acc + path.sep + cur);
+
+      try {
+        fs.statSync(currentPath);
+      } catch (e) {
+        if (e.code === 'ENOENT') {
+          fs.mkdirSync(currentPath);
+        } else {
+          throw e;
+        }
+      }
+      return currentPath;
+    }, '');
+}
