36d20cfa0c1a61456c4692b71047664e739de7b7
Pong <samfunghp@gmail.com>
2021-02-28 16:17:53 +0800

    fix: typo

--- docs/js/scrollbar.js
+++ docs/js/scrollbar.js
@@ -1,17 +1,17 @@
 // https://stackoverflow.com/a/13382873
 function getScrollbarWidth() {
   // Creating invisible container
-  const outer = document.createElement('div');
+  var outer = document.createElement('div');
   outer.style.visibility = 'hidden';
   outer.style.overflow = 'scroll'; // forcing scrollbar to appear
   document.body.appendChild(outer);
 
   // Creating inner element and placing it in the container
-  const inner = document.createElement('div');
+  var inner = document.createElement('div');
   outer.appendChild(inner);
 
   // Calculating difference between container's full width and the child width
-  const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
+  var scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
 
   // Removing temporary elements from the DOM
   outer.parentNode && outer.parentNode.removeChild(outer);
